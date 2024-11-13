6044d2ee21f3427125c3b09b22a0bc1fc0c52951
Pedro Tomasini <pedrotomasini1@outlook.com>
2024-11-13 15:50:44 -0300

    edição de fotografia

--- galeria/urls.py
+++ galeria/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path('', index, name='index'),
     path('imagem/<int:foto_id>', imagem, name='imagem'),
     path('buscar', buscar, name='buscar'),
-    path('nova_imagem', nova_imagem, name='nova_imagem'),
-    path('editar/<int:foto_id>', editar, name='editar'),
-    path('deletar/<int:foto_id>', deletar, name='deletar'),
+    path('nova-imagem', nova_imagem, name='nova_imagem'),
+    path('editar-imagem/<int:foto_id>', editar_imagem, name='editar_imagem'),
+    path('deletar-imagem/<int:foto_id>', deletar_imagem, name='deletar_imagem'),
 ]
