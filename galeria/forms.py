1e95bd96ec6b079a9aa7bac684469dc4e04d32a6
Pedro Tomasini <pedrotomasini1@outlook.com>
2024-11-13 14:45:28 -0300

    lógica de novo item

--- galeria/forms.py
+++ galeria/forms.py
@@ -6,4 +6,23 @@ class FotografiaForms(forms.ModelForm):
     class Meta:
         model = Fotografia
         exclude = ['publicada',]
-        fields = '__all__'
+        widgets = {
+            'nome': forms.TextInput(attrs={'class':'form-control'}),
+            'legenda': forms.TextInput(attrs={'class':'form-control'}),
+            'categoria': forms.Select(attrs={'class':'form-control'}),
+            'descricao': forms.Textarea(attrs={'class':'form-control'}),
+            'foto': forms.FileInput(attrs={'class':'form-control'}),
+            'data_fotografia': forms.DateInput(
+                format = '%d/%m/%Y',
+                attrs={
+                    'type':'date',
+                    'class':'form-control'
+                }
+            ),
+            'usuario': forms.Select(attrs={'class':'form-control'}),
+        }
+        labels = {
+            'descricao': 'Descrição',
+            'data_fotografia': 'Data de registro',
+            'usuario': 'Usuário',
+        }
